a6da6179dd379ce7c9365e43e2ca1fc46d91cafa
tlars925 <tlars925@mtroyal.ca>
2026-04-07 16:19:48 +0000

    Steps 1-6 complete, try out some more tests later

--- src/main/LootManager.java
+++ src/main/LootManager.java
@@ -1,7 +1,10 @@
 package main;
 
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.PrintWriter;
 
 /**
  * Manages the inventory of RPG Loot.
@@ -13,6 +16,43 @@ public class LootManager {
         this.inventory = new ArrayList<>();
     }
 
+    public static LootManager load(String filePath) throws FileNotFoundException {
+        LootManager looty = new LootManager();
+
+        Scanner scanner = new Scanner(new File(filePath));
+
+        scanner.nextLine();
+
+        while (scanner.hasNextLine()) {
+            String[] lootParts = scanner.nextLine().split(",");
+
+            Loot loot = LootFactory.create(lootParts);
+            looty.add(loot);
+        }
+
+        scanner.close();
+
+        return looty;
+    }
+
+    public void save(String filePath) throws FileNotFoundException {
+        PrintWriter writer = new PrintWriter(new File(filePath));
+
+        writer.println("TYPE,NAME,RARITY,SPECIAL_1");
+
+        for (Loot currLoot : inventory) {
+            writer.println(currLoot.asCsvRecord());
+        }
+
+        writer.close();
+    }
+
+    public void add(Loot loot) {
+        if (loot != null) {
+            inventory.add(loot);
+        }
+    }
+
     /**
      * Polymorphically displays all items in the inventory.
      */
@@ -21,7 +61,7 @@ public class LootManager {
         System.out.println("--- Current Inventory ---");
         for (Loot item : inventory) {
             System.out.println(item.getName() + " [" + item.getRarity() + "] - " +
-            item.getEffectDescription());
+                    item.getEffectDescription());
         }
         System.out.println("-------------------------");
         System.out.println();
